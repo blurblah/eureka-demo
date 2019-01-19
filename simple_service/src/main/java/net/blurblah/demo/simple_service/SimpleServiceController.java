0c5f039f0d10fa6252a1ce833ae30980ff49de15
blurblah <blurblah@blurblah.net>
2019-01-19 13:06:33 +0900

    Initial commit

---
+++ simple_service/src/main/java/net/blurblah/demo/simple_service/SimpleServiceController.java
@@ -0,0 +1,14 @@
+package net.blurblah.demo.simple_service;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleServiceController {
+
+    @RequestMapping(value="/hello", method= RequestMethod.GET)
+    public String hello() {
+        return "eureka demo";
+    }
+}
